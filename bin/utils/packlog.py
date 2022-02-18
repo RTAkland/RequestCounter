00e3521c7cb1844b693ecf68dc2b0a1a87932fcc
MarkusJoe <markushammered@gmail.com>
2022-02-18 19:35:28 +0800

    将日志文件打包为tar.gz压缩文件

--- bin/utils/packlog.py
+++ bin/utils/packlog.py
@@ -4,3 +4,26 @@
 # @Development Tool: PyCharm
 # @Create Time: 2022/2/18
 # @File Name: packlog.py
+
+
+import os
+import time
+import tarfile
+
+
+def make_targz():
+    """
+    打包为tar.gz
+    压缩文件
+    :return:
+    """
+    output_dir = f'./static/cache/{time.strftime("%Y-%m-%d %H")}.tar.gz'
+    tar = tarfile.open(output_dir, 'w:gz')
+    for root, dir, files in os.walk('./bin/log'):
+        for file in files:
+            pathfile = os.path.join(root, file)
+            tar.add(pathfile)
+    tar.close()
+
+
+__all__ = ['make_targz']
