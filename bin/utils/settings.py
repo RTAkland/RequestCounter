9e8c130e819f40ea44fb3908e7e2311ac029f454
MarkusJoe <markushammered@gmail.com>
2022-02-25 18:28:54 +0800

    添加了配置文件内MySQL数据库的配置信息的读取支持

--- bin/utils/settings.py
+++ bin/utils/settings.py
@@ -7,6 +7,7 @@
 
 
 import yaml
+from typing import Dict
 
 
 class Settings:
@@ -16,6 +17,13 @@ class Settings:
             self.data = yaml.load(fp.read(), Loader=yaml.Loader)
         self.__servers = self.data['servers']
         self.__database = self.data['database']
+
+    def all_data(self) -> Dict:
+        """
+        获取原始配置文件
+        :return:
+        """
+        return self.data
 
     @property
     def host(self) -> str:
@@ -47,7 +55,7 @@ class Settings:
         数据库路径
         :return:
         """
-        return self.__database['sqlite']['path']
+        return self.__database['SQLite']['path']
 
     @property
     def name(self) -> str:
@@ -55,4 +63,45 @@ class Settings:
         数据库名称
         :return:
         """
-        return self.__database['sqlite']['path'].split('/')[-1]
+        return self.__database['SQLite']['path'].split('/')[-1]
+
+    @property
+    def mysql_host(self) -> str:
+        """
+        mysql数据库的地址
+        :return: 
+        """
+        return self.__database['MySQL']['host']
+
+    @property
+    def mysql_port(self) -> int:
+        """
+        mysql数据库的端口
+        默认3306
+        :return:
+        """
+        return self.__database['MySQL']['port']
+
+    @property
+    def mysql_user(self) -> str:
+        """
+        mysql数据库的用户名
+        :return:
+        """
+        return self.__database['MySQL']['user']
+
+    @property
+    def mysql_pwd(self) -> str:
+        """
+        mysql数据库的密码
+        :return:
+        """
+        return self.__database['MySQL']['password']
+
+    @property
+    def mysql_db(self) -> str:
+        """
+        保存数据库数据库名
+        :return:
+        """
+        return self.__database['MySQL']['db']
