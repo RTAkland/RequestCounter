099644599be6ea9a3c71cc0ee117724015d5895c
MarkusJoe <markushammered@gmail.com>
2022-04-07 21:12:33 +0800

    添加了HTTPS跳转支持, 现在访问http://的请求均会被跳转到https://

--- app/config.py
+++ app/config.py
@@ -6,9 +6,13 @@
 # @File Name: config.py
 
 
+import os
+
+
 class Config:
     JSON_SORT_KEYS = False
     JSON_AS_ASCII = False
+    SSL_REDIRECT = False if not os.environ.get('DYNO') else True
 
     @staticmethod
     def init_app(app):
