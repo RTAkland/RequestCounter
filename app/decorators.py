7b31775a6b5fe76bcf209c2d27511e5e6a320782
MarkusJoe <markushammered@gmail.com>
2022-04-07 21:53:04 +0800

    添加了计时器装饰器

--- app/decorators.py
+++ app/decorators.py
@@ -12,11 +12,11 @@ from functools import wraps
 def time_it(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print('数据库文件开始下载')
+        print('I: 数据库文件开始下载')
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print('下载结束, 耗时 {} 秒'.format(round(end - start, 2)))
+        print('I: 下载结束, 耗时 {} 秒'.format(round(end - start, 2)))
         return result
 
     return wrapper
