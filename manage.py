95f5521af5ea297b1d4bd53dbb4a612ffdfbbc3b
MarkusJoe <markushammered@gmail.com>
2022-04-24 21:32:34 +0800

    将配置类型从默认修改为了根据环境变量来设置

--- manage.py
+++ manage.py
@@ -6,20 +6,22 @@
 # @File Name: manage.py
 
 
+import os
 from app import create_app
 
-app = create_app('default')
+
+config = os.getenv('FLASK_CONFIG') or os.getenv('DYNO') or 'default'
+app = create_app(config)
 
 if __name__ == '__main__':
-    from app import logger
     from gevent import pywsgi
 
-    logger.info('服务运行在 http://127.0.0.1:5000')
+    app.logger.info('服务运行在 http://127.0.0.1:5000')
     try:
         server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
         server.serve_forever()
     except KeyboardInterrupt:
-        logger.info('已退出')
+        app.logger.info('已退出')
     except OSError:
-        logger.critical('5000 端口已被占用')
+        app.logger.critical('5000 端口已被占用')
         exit(1)
