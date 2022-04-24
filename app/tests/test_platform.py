e62549773b841323040fc438d9a95d5d51b0362d
MarkusJoe <markushammered@gmail.com>
2022-04-24 21:30:15 +0800

    添加了系统类型测试文件

---
+++ app/tests/test_platform.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -- coding:utf-8 --
+# @Author: markushammered@gmail.com
+# @Development Tool: Pycharm
+# @Create Time: 2022/4/24
+# @File Name: test_platform.py
+
+
+import platform
+
+print(platform.system())  # Linux
